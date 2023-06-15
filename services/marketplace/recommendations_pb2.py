0e42554412cc08ab4976168ecbae659af3dd94e1
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-06-15 14:11:13 +0300

    Update grpc files, add module test for recommendation

--- services/marketplace/recommendations_pb2.py
+++ services/marketplace/recommendations_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15recommendations.proto\"^\n\x15RecommendationRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\x05\x12\x1f\n\x08\x63\x61tegory\x18\x02 \x01(\x0e\x32\r.BookCategory\x12\x13\n\x0bmax_results\x18\x03 \x01(\x05\"/\n\x12\x42ookRecommendation\x12\n\n\x02id\x18\x01 \x01(\x05\x12\r\n\x05title\x18\x02 \x01(\t\"F\n\x16RecommendationResponse\x12,\n\x0frecommendations\x18\x01 \x03(\x0b\x32\x13.BookRecommendation*?\n\x0c\x42ookCategory\x12\x0b\n\x07MYSTERY\x10\x00\x12\x13\n\x0fSCIENCE_FICTION\x10\x01\x12\r\n\tSELF_HELP\x10\x02\x32O\n\x0fRecommendations\x12<\n\tRecommend\x12\x16.RecommendationRequest\x1a\x17.RecommendationResponseb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15recommendations.proto\"^\n\x15RecommendationRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\x05\x12\x1f\n\x08\x63\x61tegory\x18\x02 \x01(\x0e\x32\r.BookCategory\x12\x13\n\x0bmax_results\x18\x03 \x01(\x05\"/\n\x12\x42ookRecommendation\x12\n\n\x02id\x18\x01 \x01(\x05\x12\r\n\x05title\x18\x02 \x01(\t\"F\n\x16RecommendationResponse\x12,\n\x0frecommendations\x18\x01 \x03(\x0b\x32\x13.BookRecommendation*i\n\x0c\x42ookCategory\x12\x19\n\x15\x42OOK_CATEGORY_MYSTERY\x10\x00\x12!\n\x1d\x42OOK_CATEGORY_SCIENCE_FICTION\x10\x01\x12\x1b\n\x17\x42OOK_CATEGORY_SELF_HELP\x10\x02\x32O\n\x0fRecommendations\x12<\n\tRecommend\x12\x16.RecommendationRequest\x1a\x17.RecommendationResponseb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'recommendations_pb2', globals())
@@ -21,13 +21,13 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _BOOKCATEGORY._serialized_start=242
-  _BOOKCATEGORY._serialized_end=305
+  _BOOKCATEGORY._serialized_end=347
   _RECOMMENDATIONREQUEST._serialized_start=25
   _RECOMMENDATIONREQUEST._serialized_end=119
   _BOOKRECOMMENDATION._serialized_start=121
   _BOOKRECOMMENDATION._serialized_end=168
   _RECOMMENDATIONRESPONSE._serialized_start=170
   _RECOMMENDATIONRESPONSE._serialized_end=240
-  _RECOMMENDATIONS._serialized_start=307
-  _RECOMMENDATIONS._serialized_end=386
+  _RECOMMENDATIONS._serialized_start=349
+  _RECOMMENDATIONS._serialized_end=428
 # @@protoc_insertion_point(module_scope)
