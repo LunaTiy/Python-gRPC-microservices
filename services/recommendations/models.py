9f903a785560fa397d6733dbe033d3aa71ff836f
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-22 14:22:31 +0300

    feat: update models, connection string to db

--- services/recommendations/models.py
+++ services/recommendations/models.py
@@ -1,11 +1,30 @@
-from sqlalchemy import Column, Integer, Text
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, ForeignKeyConstraint, Integer, PrimaryKeyConstraint, String
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 
-class Book(Base):
-    __tablename__ = 'books'
+class Categories(Base):
+    __tablename__ = 'categories'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='categories_pkey'),
+    )
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    book = Column(Text, nullable=False)
+    id = Column(Integer)
+    category_name = Column(String(255), nullable=False)
+
+    books = relationship('Books', back_populates='category')
+
+
+class Books(Base):
+    __tablename__ = 'books'
+    __table_args__ = (
+        ForeignKeyConstraint(['category_id'], ['categories.id'], name='books_category_id_fkey'),
+        PrimaryKeyConstraint('id', name='books_pkey')
+    )
+
+    id = Column(Integer)
+    book_name = Column(String(255), nullable=False)
+    category_id = Column(Integer, nullable=False)
+
+    category = relationship('Categories', back_populates='books')
--- services/recommendations/sa_utils.py
+++ services/recommendations/sa_utils.py
@@ -4,9 +4,9 @@ from typing import Iterator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-from services.recommendations.models import Book
+from services.recommendations.models import Books
 
-engine = create_engine("postgresql+psycopg2://postgres:qwerty123@localhost:5432/test-db", echo=False)
+engine = create_engine("postgresql+psycopg2://postgres:qwerty123@localhost:5432/marketplace", echo=False)
 session_maker = sessionmaker(bind=engine, expire_on_commit=False)
 
 
@@ -25,5 +25,5 @@ def session_scope() -> Iterator[Session]:
 
 if __name__ == "__main__":
     with session_scope() as s:
-        book = s.query(Book).filter(Book.id == 1).first()
+        book = s.query(Books).filter(Books.category_id == 1).all()
         pass
